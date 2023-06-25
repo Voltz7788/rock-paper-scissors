ead6f6fc1e088aae15cdfab17fc3e2fe9c5c93bb
Yannis <yannismadu777@gmail.com>
2023-06-25 15:22:36 +0100

    Create decisionMaker function
    This function compares the player and CPU choices to see who wins that
    round.

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 
 
 function getPlayerChoice() {
-    playerChoice = prompt("Rock, paper or scissors?:").toLowerCase()
+    playerChoice = prompt('Rock, paper or scissors?:').toLowerCase()
     if (playerChoice === 'rock' 
         || playerChoice === 'paper' 
         || playerChoice === 'scissors') {
@@ -25,5 +25,37 @@ function getPlayerChoice() {
 }
 
 function playRound(cpuChoice, playerChoice) {
+
+    // If player chooses 'rock' conditions
+    if (playerChoice === 'rock') {
+        if (cpuChoice === 'rock') {
+            alert('It\'s a tie!')
+        } else if (cpuChoice === 'scissors') {
+            alert('You win!')
+        } else if (cpuChoice === 'paper') {
+            alert('You lose!')
+        }    
+    } 
     
+    // If player chooses 'paper' conditions
+    else if (playerChoice === 'paper') {
+        if (cpuChoice === 'paper') {
+            alert('It\'s a tie!')
+        } else if (cpuChoice === 'rock') {
+            alert('You win!')
+        } else if (cpuChoice === 'scissors') {
+            alert('You lose!')
+        }
+    } 
+    
+    // If player chooses 'scissors' conditions
+    else if (playerChoice === 'scissors') {
+        if (cpuChoice === 'scissors') {
+            alert('It\'s a tie!')
+        } else if (cpuChoice === 'paper') {
+            alert('You win!')
+        } else if (cpuChoice === 'rock') {
+            alert('You lose!')
+        }
+    }
 }
